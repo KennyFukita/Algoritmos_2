d8b6c8f2e4d51c12a68a9fd8b6099772feb997ea
Kenny Fukita <kennyfukita@gmail.com>
2020-03-03 12:31:27 -0300

    Exercicios 4 e 5 da lista 1
    Exercicios 4 e 5 da lista 1

--- Lista 01/ex04.c
+++ Lista 01/ex04.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Ler dois números inteiros e imprimir a soma.*/
+
 int main (){
-	int x1, x2;
+	int x1, x2, soma;
 	
 	printf("Informe o primeiro valor: ");
 	scanf("%d",&x1);
 	printf("Informe o segundo valor: ");
 	scanf("%d",&x2);
-	printf("Primeiro valor: %d \n",x1);
-	printf("Segundo valor: %d \n",x2);
-
+	soma = x1+x2;
+	printf("Soma dos valores: %d \n",soma);
 
 system("pause");
 return 0;
